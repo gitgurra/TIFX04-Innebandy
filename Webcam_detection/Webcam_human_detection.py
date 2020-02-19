46c6992c3a5e7d12702861c59572ff6a1d47cf10
gitgurra <55788928+gitgurra@users.noreply.github.com>
2020-02-19 09:32:00 +0100

    Update Webcam_human_detection.py

--- Webcam_detection/Webcam_human_detection.py
+++ Webcam_detection/Webcam_human_detection.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+from scipy.spatial import distance as distance
+from collections import OrderedDict
+import CentroidTracker
 
 # Initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -8,11 +11,12 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cv2.startWindowThread()
 
 # Open webcam video stream 0 for built-in webcam, 1 for external webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)
 
 # The output will be written to webcam_output.avi
 out = cv2.VideoWriter("webcam_output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 15., (640, 480))
-
+ct = CentroidTracker.CentroidTracker()
+(H, W) = (None, None)
 
 while(True):
     # Reading the frame
@@ -23,14 +27,38 @@ while(True):
     # Using a greyscale picture, also for faster detection
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+	# construct a blob from the frame, pass it through the network,
+	# obtain our output predictions, and initialize the list of
+	# bounding box rectangles
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H), (104.0, 177.0, 123.0))
+    #net.setInput(blob)
+    #detections = net.forward()
+    rects = []
+
     # Detect people in the image, returns the bounding boxes for the detected objects
     boxes, weights = hog.detectMultiScale(frame, winStride=(8,8))
 
     boxes = np.array([[x, y, x+w, y+h] for (x, y, w, h) in boxes])
+    print("running...")
 
-    for (xA, yA, xB, yB) in boxes:
-        # Display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	# if the frame dimensions are None, grab them
+    
+    for i in boxes:
+
+        rects.append(i)
+        cv2.rectangle(frame, i, (0, 255, 0), 2)
+
+    objects = ct.update(rects)
+	# loop over the tracked objects
+    for (objectID, centroid) in objects.items():
+        # draw both the ID of the object and the centroid of the
+		# object on the output frame
+        text = "ID {}".format(objectID)
+        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
 
     # Write the output video
     out.write(frame.astype("uint8"))
